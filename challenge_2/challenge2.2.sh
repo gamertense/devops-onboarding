e4871602fa0e47dbe9336fd56c5f2acb410b6387
Vatcharat Rattananun <ict2jack@gmail.com>
2024-06-11 15:12:40 +0700

    fix path

--- challenge_2/challenge2.2.sh
+++ challenge_2/challenge2.2.sh
@@ -1,4 +1,4 @@
-cd ./challenge_1/example_directory/config
+cd ../challenge_1/example_directory/config
 # Find all .conf files, search for lines containing "server", sort the results, and save to servers.txt
 find . -name "*.conf" | xargs grep "server" | sort > servers.txt
 cat servers.txt
